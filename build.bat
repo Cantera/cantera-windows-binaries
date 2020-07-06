65e2f3d15b19ca487165d5224a47f74c4f882fa9
Bryan W. Weber <bryan.w.weber@gmail.com>
2020-07-06 15:27:30 -0400

    Updates for 2.5.0

--- build.bat
+++ build.bat
@@ -6,14 +6,6 @@ IF %BUILD_ARCH% EQU 64 (
    SET ARCH_NAME="x86"
 )
 
-:: The major version of Python being used
-SET PY_MAJ_VER=%PY_VER:~0,1%
-
-IF %PY_MAJ_VER% EQU 2 (
-   CALL pip install 3to2
-   CALL %PYTHON3_LOC%\Scripts\pip install numpy==1.14.5
-)
-
 :: Set the number of CPUs to use in building
 SET CPU_USE=2
 
@@ -27,9 +19,9 @@ git checkout %GIT_COMMIT%
 :: Have to use CALL to prevent the script from exiting after calling SCons
 CALL scons clean
 
-:: Put important settings into cantera.conf for the build. Use VS 2015 to
+:: Put important settings into cantera.conf for the build. Use VS 2019 to
 :: compile the interface.
-ECHO msvc_version='14.0' >> cantera.conf
+ECHO msvc_version='14.2' >> cantera.conf
 IF "%BUILD_MATLAB%"=="Y" (
 ECHO matlab_toolbox='y' >> cantera.conf
 ECHO matlab_path='%CD%/../mw_headers' >> cantera.conf
@@ -39,30 +31,19 @@ ECHO matlab_toolbox='n' >> cantera.conf
 ECHO debug='n' >> cantera.conf
 ECHO f90_interface='n' >> cantera.conf
 ECHO system_sundials='n' >> cantera.conf
+ECHO python_package='full' >> cantera.conf
 
-IF %PY_MAJ_VER% EQU 2 (
-    ECHO python3_cmd='%PYTHON3_LOC%/python.exe' >> cantera.conf
-    ECHO python3_package='y' >> cantera.conf
-)
+ECHO boost_inc_dir="C:/Libraries/boost_1_73_0" >> cantera.conf
 
-SET "ESC_PREFIX=%PREFIX:\=/%"
-ECHO boost_inc_dir="C:/Libraries/boost_1_67_0" >> cantera.conf
-
-CALL scons build -j%CPU_USE% python_package=y
+CALL scons build -j%CPU_USE%
 CALL scons msi
 
 dir
 
 IF %BUILD_ARCH% EQU 64 (
    move Cantera-%CT_VERSION%.win-amd64-py%PY_VER%.msi Cantera-Python-%CT_VERSION%-x64-py%PY_VER%.msi
-   IF %PY_MAJ_VER% EQU 2 (
-      move Cantera-%CT_VERSION%.win-amd64-py3.4.msi Cantera-Python-%CT_VERSION%-x64-py3.4.msi
-   )
 ) ELSE (
    move Cantera-%CT_VERSION%.win32-py%PY_VER%.msi Cantera-Python-%CT_VERSION%-x86-py%PY_VER%.msi
-   IF %PY_MAJ_VER% EQU 2 (
-      move Cantera-%CT_VERSION%.win32-py3.4.msi Cantera-Python-%CT_VERSION%-x86-py3.4.msi
-   )
 )
 
 if "%BUILD_MATLAB%"=="Y" (
