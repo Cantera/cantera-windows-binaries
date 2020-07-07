baf61911dcd70fd8e9305c4f3bec44566a5b1cd0
Bryan W. Weber <bryan.w.weber@gmail.com>
2020-07-06 21:11:35 -0400

    Try out GitHub Actions

--- build.bat
+++ build.bat
@@ -1,20 +1,7 @@
-IF %BUILD_ARCH% EQU 64 (
-   CALL "%VS140COMNTOOLS%"\..\..\VC\bin\amd64\vcvars64.bat
-   SET ARCH_NAME="x64"
-) ELSE (
-   CALL "%VS140COMNTOOLS%"\..\..\VC\bin\vcvars32.bat
-   SET ARCH_NAME="x86"
-)
-
 :: Set the number of CPUs to use in building
 SET CPU_USE=2
 
-IF "%BUILD_MATLAB%"=="Y" (
-git clone https://cantera:%GIT_PW%@cantera.org/mw_headers.git
-)
-git clone https://github.com/Cantera/cantera.git
 cd cantera
-git checkout %GIT_COMMIT%
 
 :: Have to use CALL to prevent the script from exiting after calling SCons
 CALL scons clean
@@ -24,7 +11,7 @@ CALL scons clean
 ECHO msvc_version='14.2' >> cantera.conf
 IF "%BUILD_MATLAB%"=="Y" (
 ECHO matlab_toolbox='y' >> cantera.conf
-ECHO matlab_path='%CD%/../mw_headers' >> cantera.conf
+ECHO matlab_path='%MW_HEADERS_DIR%' >> cantera.conf
 ) ELSE (
 ECHO matlab_toolbox='n' >> cantera.conf
 )
@@ -33,21 +20,21 @@ ECHO f90_interface='n' >> cantera.conf
 ECHO system_sundials='n' >> cantera.conf
 ECHO python_package='full' >> cantera.conf
 
-ECHO boost_inc_dir="C:/Libraries/boost_1_73_0" >> cantera.conf
+ECHO boost_inc_dir="%BOOST_ROOT_1_69_0%" >> cantera.conf
 
 CALL scons build -j%CPU_USE%
 CALL scons msi
 
 dir
 
-IF %BUILD_ARCH% EQU 64 (
+IF "%BUILD_ARCH%"=="x64" (
    move Cantera-%CT_VERSION%.win-amd64-py%PY_VER%.msi Cantera-Python-%CT_VERSION%-x64-py%PY_VER%.msi
 ) ELSE (
    move Cantera-%CT_VERSION%.win32-py%PY_VER%.msi Cantera-Python-%CT_VERSION%-x86-py%PY_VER%.msi
 )
 
 if "%BUILD_MATLAB%"=="Y" (
-move cantera.msi "Cantera-%CT_VERSION%-%ARCH_NAME%.msi"
+move cantera.msi "Cantera-%CT_VERSION%-%BUILD_ARCH%.msi"
 ) ELSE (
 :: Only want cantera.msi from the build that includes the Matlab toolbox
 del cantera.msi
